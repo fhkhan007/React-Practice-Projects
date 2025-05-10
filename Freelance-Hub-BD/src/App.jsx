c120c67b3d839597aad0efd3e99ea3847187e16d
MD: Fahad Hossain Khan <fahadhossainkhan@gmail.com>
2025-05-10 19:05:59 +0600

    practice project freelance Hub Bd

---
+++ Freelance-Hub-BD/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style/App.css";
+import style from "./style/App.module.css";
+
+const App = () => {
+  return (
+    <>
+      <h1 className="title">Freelance Hub BD </h1>
+      <h2 className={style.home}>Home</h2>
+    </>
+  );
+};
+
+export default App;
